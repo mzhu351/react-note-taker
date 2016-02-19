fea8f801a6a97efae9dd81cef1f9214174b90b3a
Min Zhu <mzhu@financialengines.com>
2016-02-19 10:12:12 -0800

    got form

---
+++ index.ios.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  NavigatorIOS,
+  View
+} from 'react-native';
+
+var Main = require('./App/Components/Main');
+
+class githubNotetaker extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Github Notetaker',
+          component: Main,
+        }} />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111111',
+  },
+
+});
+
+AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
